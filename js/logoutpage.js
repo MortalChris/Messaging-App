c99054186d7898f562e85d131b437afb38b20c5c
Christian Valeri <mortlaxris@outlook.com>
2024-03-09 21:26:15 -0600

    change page name to logout-page

--- js/logoutpage.js
+++ js/logoutpage.js
@@ -5,14 +5,14 @@ const logOutPage = express.Router();
 const sessionMiddleware = require("./sessionMiddleware");
 logOutPage.use(sessionMiddleware);
 
-logOutPage.get("/logOutPage", (req, res) => {
+logOutPage.get("/logout-page", (req, res) => {
     res.render('logOutPage');
 });
 
 logOutPage.post("/logOutPage", async function (req, res) { 
     req.session.loggedin = false;
     req.session.username = "";
-    res.redirect("loginPage");
+    res.redirect("logout-page");
     console.log(req.session.loggedin);
 })
 
