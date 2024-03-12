253eedd01f9b9f204e61aeb160d2a92913ba5d2f
Christian Valeri <mortlaxris@outlook.com>
2024-03-12 00:11:08 -0500

    added socket io requires, change listen to server instead of app

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ const app = express();
 const port = 3000;
 const path = require("path");
 app.use(express.urlencoded({ extended: false }));
-// ejs also allows normal htmml file instead of ejs file
 // const ejs = require('ejs');
-app.engine('.html', require('ejs').__express);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+app.engine('.html', require('ejs').__express); //tells Express to use EJS as the template engine for files with the .html extension
+app.set('views', path.join(__dirname, 'views')); //sets the directory where Express will look for view files
+app.set('view engine', 'html');// sets the default view engine to use when rendering views (In this case its set to html)
 //Public Routes (css)
 app.use(express.static(path.join(__dirname, 'public')));
+//Socket
+const http = require('http');
+const socketIO = require('socket.io');
+const server = http.createServer(app);
+const io = socketIO(server);
+// Attach the Socket.IO instance to the Express app
+app.io = io;
 
 //Routes
 const homepageRouter = require("./js/routes/homepage");
@@ -26,6 +32,6 @@ app.use("/", signUpPageRouter);
 app.use("/", chatPageRouter);
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}/homepage`);
 });
