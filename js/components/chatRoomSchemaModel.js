2a0acd0719a64ea9acfe08e25ae911a99c01eadd
Christian V. <88056810+MortalChris@users.noreply.github.com>
2024-04-09 16:18:17 +0000

    restructured chatroomModel to be in array containing an object, added timestamp

--- js/components/chatRoomSchemaModel.js
+++ js/components/chatRoomSchemaModel.js
@@ -3,13 +3,12 @@ const mongoose = require('mongoose');
 
 const ChatRoomModal = mongoose.model('ChatRoom', new mongoose.Schema({
     // Define your data schema here
-    room: {type: String, required: true},
-    chat:{ 
-        username: {type: String, required: true},
-        message: {type: String, required: true}
-    }
-    // user: { type: String, required: true},
-    // message: { type: String, required: true }
+    room: { type: String, required: true },
+    messages: [{
+        username: { type: String, required: true },
+        message: { type: String, required: true },
+        timestamp: { type: Date, default: Date.now }
+    }]
 }));
 
 module.exports = ChatRoomModal;
