e2f87481be0a8d9c465dc19940c99ad6d6c7dc3e
Christian Valeri <mortlaxris@outlook.com>
2024-05-03 17:54:46 -0500

    change parent messages to chat and message child to messages

--- js/components/chatRoomSchemaModel.js
+++ js/components/chatRoomSchemaModel.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 const ChatRoomModal = mongoose.model('ChatRoom', new mongoose.Schema({
     // Define your data schema here
     room: { type: String, required: true },
-    messages: [{
+    chat: [{
         sender: { type: String, required: true },
-        message: { type: String, required: true },
+        messages: { type: Array, required: true },
         timestamp: { type: Date, default: Date.now }
     }]
 }));
