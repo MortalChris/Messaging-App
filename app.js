2fb4bd0509525db5ddcb6a468b41475ea36c6e68
Christian Valeri <mortlaxris@outlook.com>
2024-06-25 17:41:29 -0500

    Commented out all of socketIO code to see if its the reason why text is being displayed from MongoDB

--- app.js
+++ app.js
@@ -39,91 +39,95 @@ app.use("/", chatPageRouter);
     const ChatModel = require("./js/components/chatRoomSchemaModel");
 
 
-//Socket IO
-const { createServer } = require('node:http');
-const { Server } = require('socket.io');
-const server = createServer(app);
-const io = new Server(server, {
-    connectionStateRecovery: {}//temporarily store all the events that are sent by the server and will try to restore the state of a client when it reconnects
-});
+// Socket IO
+// const { createServer } = require('node:http');
+// const { Server } = require('socket.io');
+// const server = createServer(app);
+// const io = new Server(server, {
+//     connectionStateRecovery: {}//temporarily store all the events that are sent by the server and will try to restore the state of a client when it reconnects
+// });
 
 
-io.on('connection', async (socket) => {
-    connectionStateRecovery: {}
-    console.log(`user ${sessionMiddleware.username} connected`); //${socket.id.substring(0, 5)}
-    const data = await ChatModel.find({}).exec();//if this doesn't work move inside enter room socket, remove async from io.on
+// io.on('connection', async (socket) => {
+//     connectionStateRecovery: {}
+//     console.log(`user ${sessionMiddleware.username} connected`); //${socket.id.substring(0, 5)}
+//     const data = await ChatModel.find({}).exec();//if this doesn't work move inside enter room socket, remove async from io.on
 
-    //Join room
-    socket.on('enterRoom', async ({ username, room }) => {
-        // Check if the room exists
-        let chatRoom = await ChatModel.findOne({ roomId: room }).exec();
-        if(!chatRoom){//If chatRoom doesnt exist make one
-            const chat = new ChatModel({//Uploads to database
-                room: room,
-                chat:{ 
-                    sender: username,
-                    messages: []
-                }
-            });
-            await chat.save();
-        }
+//     //Join room
+//     socket.on('enterRoom', async ({ username, room }) => {
+//         // Check if the room exists
+//         let chatRoom = await ChatModel.findOne({ roomId: room }).exec();
+//         if(!chatRoom){//If chatRoom doesnt exist make one
+//             const chat = new ChatModel({//Uploads to database
+//                 room: room,
+//                 chat:{
+//                     sender: username,
+//                     messages: []
+//                 }
+//             });
+//             await chat.save();
+//         }
 
-        // Join the room
-        // data.forEach(data => {
-            socket.join(room);//data.room
-            console.log(`User ${data.sender} has joined room ${room}`);//data.room
-            // Optionally, send a confirmation back to the client
-            socket.emit('message', `You have joined room ${room}`);//data.room
-        // });
-    });
+//         // Join the room
+//         // data.forEach(data => {
+//             socket.join(room);//data.room
+//             console.log(`User ${data.sender} has joined room ${room}`);//data.room
+//             // Optionally, send a confirmation back to the client
+//             socket.emit('message', `You have joined room ${room}`);//data.room
+//         // });
+//     });
 
-    //When a user connects (only to user)
-    socket.emit('message', `Welcome to the chat!`)
+//     //When a user connects (only to user)
+//     socket.emit('message', `Welcome to the chat!`)
 
-    //When a user connects (to everyone but user)
-    socket.broadcast.emit('message', `User ${sessionMiddleware.username} connected`); //${socket.id.substring(0, 5)}
-
-
-    //Sending a message
-    socket.on('chat message', async ({ msg, room, username }) => {// grabs submitted room and submitted message
-
-        const findRoom = await ChatModel.findOne({ room: room });
-        console.log(room)
-        console.log("chat message socket: findRoom = " + findRoom)
-        try {
-            if(findRoom){
-                //Adds messages to array object
-
-                findRoom.chat.push({ sender: username, messages: msg, timestamp: new Date() });
-                await findRoom.save();
-                //await findRoom.save();
-                console.log(findRoom);
-                // data.forEach(data => {
-                //     io.to(data.room).emit('chat message', { msg: data.msg, sender: data.sender });
-                // });
-            }
-        } catch {
-            console.log("An error has occured with submitting room")
-        }
-
-        io.to(room).emit('chat message', { msg, username }); // Broadcast the message to all connected clients
-        console.log(`Sent message in room: ${room}. Msg: ${msg}`);
-    });    
+//     //When a user connects (to everyone but user)
+//     socket.broadcast.emit('message', `User ${sessionMiddleware.username} connected`); //${socket.id.substring(0, 5)}
 
 
-    socket.on('user disconnect', function ({room, username}) {
-        console.log(`User ${username} disconnected`);////socket.id.substring(0, 5)}
-        //When a user disconnects (to everyone but user)
-        socket.broadcast.to(room).emit('message', `User ${username} disconnected`);//socket.id.substring(0, 5)}
-    });
+//     //Sending a message
+//     socket.on('chat message', async ({ msg, room, username }) => {// grabs submitted room and submitted message
 
-    //Listens for activity (to everyone but user)
-    socket.on('activity', ({ name, room }) => {//these name things might be uneccessary now
-        socket.broadcast.to(room).emit('activity', name);
-    });
-})
+//         const findRoom = await ChatModel.findOne({ room: room });
+//         console.log(room)
+//         console.log("chat message socket: findRoom = " + findRoom)
+//         try {
+//             if(findRoom){
+//                 //Adds messages to array object
+
+//                 findRoom.chat.push({ sender: username, messages: msg, timestamp: new Date() });
+//                 await findRoom.save();
+//                 //await findRoom.save();
+//                 console.log(findRoom);
+//                 // data.forEach(data => {
+//                 //     io.to(data.room).emit('chat message', { msg: data.msg, sender: data.sender });
+//                 // });
+//             }
+//         } catch {
+//             console.log("An error has occured with submitting room")
+//         }
+
+//         io.to(room).emit('chat message', { msg, username }); // Broadcast the message to all connected clients
+//         console.log(`Sent message in room: ${room}. Msg: ${msg}`);
+//     });
 
 
-    server.listen(port, () => {
+//     socket.on('user disconnect', function ({room, username}) {
+//         console.log(`User ${username} disconnected`);////socket.id.substring(0, 5)}
+//         //When a user disconnects (to everyone but user)
+//         socket.broadcast.to(room).emit('message', `User ${username} disconnected`);//socket.id.substring(0, 5)}
+//     });
+
+//     //Listens for activity (to everyone but user)
+//     socket.on('activity', ({ name, room }) => {//these name things might be uneccessary now
+//         socket.broadcast.to(room).emit('activity', name);
+//     });
+// })
+
+
+    // server.listen(port, () => {
+    //     console.log(`Example app listening on port http://localhost:${port}/homepage`);
+// });
+
+    app.listen(port, () => {
         console.log(`Example app listening on port http://localhost:${port}/homepage`);
-    });
+});  
